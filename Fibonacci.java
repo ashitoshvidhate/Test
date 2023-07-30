f983551a75b87666a12553f4f5a91020cf61f5f3
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-07-30 18:59:40 +0530

    Commit 2 : Update java file

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,12 +3,23 @@ public class Fibonacci {
     public static void main(String[] args) {
         int n = 20;
         int a = 0, b = 1;
-        System.out.print("Fibonacci Series with " + n + " numbers: ");
+        System.out.print("Prime Numbers with " + n + " numbers: ");
         for (int i = 1; i <= n; i++) {
-            System.out.print(a + " ");
+            if (isPrime(a))
+                System.out.print(a + " ");
             int sum = a + b;
             a = b;
             b = sum;
         }
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
     }
 }
