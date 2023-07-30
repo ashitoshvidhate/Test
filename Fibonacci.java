f72425acca6f01dad5f4093f32e78a18b2300798
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2023-07-30 19:01:03 +0530

    Commit 3: Update Java with both code

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,12 +3,24 @@ public class Fibonacci {
     public static void main(String[] args) {
         int n = 20;
         int a = 0, b = 1;
-        System.out.print("Fibonacci Series with " + n + " numbers: ");
+        System.out.print("Armstrong Numbers with " + n + " numbers: ");
         for (int i = 1; i <= n; i++) {
-            System.out.print(a + " ");
+            if (isArmstrong(a))
+                System.out.print(a + " ");
             int sum = a + b;
             a = b;
             b = sum;
         }
+    }
+
+    static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, 3);
+            num /= 10;
+        }
+        return sum == original;
     }
 }
